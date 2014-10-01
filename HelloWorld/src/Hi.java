7540325e5be385305b44f97d8df1aa31a5f970df
punit <mindtestandroid@gmail.com>
2014-10-01 12:00:30 +0530

    OK.,.,.,

--- HelloWorld/src/Hi.java
+++ HelloWorld/src/Hi.java
@@ -1,5 +1,6 @@
 public class Hi {
 	public static void main(String[] args) {
-		System.out.println("Hiiii Java how are you?");
+		System.out
+				.println(" Hiiii Java how are you?\nHope, you doing well.\nMeet you soon.");
 	}
 }
---
+++ Test123/src/Test12345.java
@@ -0,0 +1,5 @@
+public class Test12345 {
+	public static void main(String[] args) {
+		System.out.println("HIIoioioioio");
+	}
+}
